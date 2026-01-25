cdc31bb947cfd55c39e5fafcbce17c24a5e27ba0
emon972 <39545328+emon972@users.noreply.github.com>
2026-01-26 01:06:40 +0600

    Create exchanges.py

---
+++ exchanges.py
@@ -0,0 +1,22 @@
+EXCHANGES = {
+    "BitMart": "https://support.bmx.fund/hc/en-us/categories/360000132534",
+    "LBank": "https://www.lbank.com/support/announcement",
+    "XT": "https://www.xt.com/en/support/announcement",
+    "AscendEX": "https://ascendex.com/en/support/articles",
+    "Weex": "https://weex.com/announcement",
+    "Coinstore": "https://coinstore.com/announcement",
+    "Phemex": "https://phemex.com/announcement",
+    "Pionex": "https://pionex.com/announcement",
+    "LATOKEN": "https://latoken.com/blog",
+    "Poloniex": "https://poloniex.com/blog",
+    "Toobit": "https://www.toobit.com/en-US/support"
+}
+
+KEYWORDS = [
+    "will list",
+    "trading will begin",
+    "deposit open",
+    "is pleased to announce"
+]
+
+TARGET_EXCHANGE = "Biconomy"
